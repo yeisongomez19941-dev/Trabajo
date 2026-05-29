5acdb0dd0c3112b62e308a8cf1d200fa3f3c72e8
Yeison <yeisongomez19941@gmail.com>
2026-05-29 13:19:39 -0500

    Auditorias modificada

--- Libreria_Lecturas/Entidades/Auditorias.cs
+++ Libreria_Lecturas/Entidades/Auditorias.cs
@@ -4,13 +4,16 @@ using System.Text;
 
 namespace Libreria_Lecturas.Entidades
 {
-        public class Auditorias
+    namespace Libreria_Lecturas.Entidades
+    {
+        public class Auditoria
         {
             public int Id { get; set; }
-            public string? Tabla { get; set; }        // Qué entidad se modificó (Libros, Autores...)
-            public string? Accion { get; set; }       // Crear, Modificar, Eliminar
-            public string? UsuarioEmail { get; set; } // Quién lo hizo
-            public DateTime Fecha { get; set; }       // Cuándo lo hizo
-            public string? Detalle { get; set; }      // Qué cambio")
+            public string? Tabla { get; set; }        // Ej: "Libros", "Autores", "Usuarios"
+            public string? Accion { get; set; }       // "Crear", "Modificar", "Eliminar"
+            public string? UsuarioEmail { get; set; } // Quién hizo la acción
+            public DateTime Fecha { get; set; }       // Cuándo
+            public string? Detalle { get; set; }      // Ej: "Libro creado: El Principito"
         }
+    }
 }
