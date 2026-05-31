abd1556339ce29f95af0c166d6ab50d1aa18ffe5
Yeison <yeisongomez19941@gmail.com>
2026-05-31 12:07:33 -0500

    Cambios en notas, nuevas funcionalidades

--- Libreria_Lecturas/Entidades/Notas.cs
+++ Libreria_Lecturas/Entidades/Notas.cs
@@ -1,4 +1,6 @@
-﻿namespace Libreria_Lecturas.Entidades
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Libreria_Lecturas.Entidades
 {
     public class Notas
     {
@@ -6,5 +8,13 @@
         public int Pagina { get; set; }
         public string? Contenido { get; set; }
         public DateTime? Fecha { get; set; }
+        public int? UsuarioId { get; set; }
+        public int? LibroId { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Usuarios? _UsuarioId { get; set; }
+
+        [ForeignKey("LibroId")]
+        public Libros? _LibroId { get; set; }
     }
 }
---
+++ Libreria_Lecturas/Migrations/20260531170619_Notas.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Libreria_Lecturas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Notas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LibroId",
+                table: "Notas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Notas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notas_LibroId",
+                table: "Notas",
+                column: "LibroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notas_UsuarioId",
+                table: "Notas",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notas_Libros_LibroId",
+                table: "Notas",
+                column: "LibroId",
+                principalTable: "Libros",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notas_Usuarios_UsuarioId",
+                table: "Notas",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notas_Libros_LibroId",
+                table: "Notas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notas_Usuarios_UsuarioId",
+                table: "Notas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notas_LibroId",
+                table: "Notas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notas_UsuarioId",
+                table: "Notas");
+
+            migrationBuilder.DropColumn(
+                name: "LibroId",
+                table: "Notas");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Notas");
+        }
+    }
+}
